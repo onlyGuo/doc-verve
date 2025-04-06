bad09b80907e89fd3485c9545c5129627bf1365d
gsk <719348277@qq.com>
2025-04-06 15:11:45 +0800

    init

---
+++ src/main/java/com/guoshengkai/doc/KnowledgePrairieApplication.java
@@ -0,0 +1,13 @@
+package com.guoshengkai.doc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowledgePrairieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KnowledgePrairieApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/guoshengkai/doc/KnowledgePrairieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guoshengkai.doc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnowledgePrairieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
